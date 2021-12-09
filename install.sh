a83becfd19a8e5f14edbc9e48e1ec02780a4be3b
HParker <HParker@github.com>
2021-12-09 15:34:46 -0800

    don't start up emacs

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
 apt-get install emacs
 git clone git@github.com:HParker/.emacs.d.git ~/.emacs.d
 (cd ~/.emacs.d && git checkout straight-version)
-
-# This should setup straight the first time so we don't have to wait for it to load later
-emacs --kill
