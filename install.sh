ec2b7239b737e17758d6c6b46568814aaabc3be3
HParker <HParker@github.com>
2021-12-09 16:03:32 -0800

    Try just install emacs

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-sudo apt-get install emacs
-git clone git@github.com:HParker/.emacs.d.git ~/.emacs.d
-(cd ~/.emacs.d && git checkout straight-version)
+sudo apt-get install -y emacs
+# git clone git@github.com:HParker/.emacs.d.git ~/.emacs.d
+# (cd ~/.emacs.d && git checkout straight-version)
